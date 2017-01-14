8a8564423e944b8be64e90c903bae63d7acf8fbd
Drigg3r <drigg3r@yandex.com>
2017-01-14 00:16:44 -0600

    Update Scan.sh

--- Scan.sh
+++ Scan.sh
@@ -5,8 +5,8 @@ payload="//www.google.com/%2e%2e" #The payloads for open redirect
 
 cat "$target"|while read inp; do
 	m=`curl -I -k "https://$inp$payload" -s`
-	if echo "$m"|grep -i "location" |grep "google.com" -q; then
-		echo "FOUND https://$inp//www.google.com/%2e%2e"
+	if echo "$m"|grep -i "location" |grep "$payload" -q; then
+		echo "FOUND https://$inp$payload"
 		echo "---This was the response---"
 		echo "$m"
 	fi
